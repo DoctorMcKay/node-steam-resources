bc50b5c6fa8406dffcfb02a08caf898af9965e89
Alexander Corn <mckay@doctormckay.com>
2017-06-20 01:15:45 -0400

    Remove obsolete entirely from messages

--- steam_language_parser/shoehorn.js
+++ steam_language_parser/shoehorn.js
@@ -7,6 +7,6 @@ FS.readdirSync(__dirname + '/../steam_language').forEach(function(filename) {
 	}
 	
 	var file = FS.readFileSync(__dirname + '/../steam_language/' + filename).toString('ascii');
-	file = file.replace(/; removed/g, '; obsolete').replace(/\> removed/g, '> obsolete');
+	file = file.replace(/; removed/g, '; obsolete').replace(/\> removed/g, '>');
 	FS.writeFileSync(__dirname + '/../steam_language/' + filename, file);
 });
